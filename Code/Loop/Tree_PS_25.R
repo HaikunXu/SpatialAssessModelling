c647e9dc325f00ddf29471a56da703a454de250d
Haikun Xu <haikunxu1990@gmail.com>
2022-04-01 13:56:07 -0700

    add ESS=25 loop code

---
+++ Code/Loop/Tree_PS_25.R
@@ -0,0 +1,120 @@
+# Regression Tree for PS
+
+library(tidyverse)
+
+for (i in 1:10) {
+### Purse-seine
+save_dir <- paste0("D:/OneDrive - IATTC/IATTC/2021/Spatial-SA/SpatialAssessModelling/Data/Loop/",i,"/")
+load(paste0(save_dir,"PS_LF_25.RData"))
+
+LF_raw <- LF_DF %>%
+  mutate(year=ceiling(Year/4),quarter=(Year-1)%%4 +1) %>%
+  select(year,quarter,Lat,Lon,Length,LF) %>%
+  rename(lat=Lat,lon=Lon) %>%
+  group_by(year,quarter,lat,lon) %>%
+  mutate(tot=sum(LF)) %>%
+  filter(tot>0) # remove all-0 rows
+  
+# regression tree result
+reg_rt <- function(Lat,Lon) {
+  Region <- rep(2,length(Lat))
+
+  Region[which(Lat<(-5))] <- 1
+  Region[which(Lat>0)] <- 3
+
+  return(Region)
+}
+
+# load PS catch data
+load(paste0(save_dir,"PS_Catch_25.RData"))
+Catch <- Catch_DF %>%
+  mutate(year=ceiling(Year/4),quarter=(Year-1)%%4 +1) %>%
+  rename(lat=Lat,lon=Lon) %>%
+  select(year,quarter,lat,lon,Catch)
+
+Catch$lat <- as.numeric(levels(Catch$lat))[Catch$lat]
+Catch$lon <- as.numeric(levels(Catch$lon))[Catch$lon]
+
+Catch$Cell <- reg_rt(Catch$lat,Catch$lon)
+
+# combine the flag with catch data and then group catch
+Catch_Fishery <- Catch %>%
+  group_by(year,quarter,Cell) %>%
+  summarise(Total_Catch=sum(Catch)) %>%
+  mutate(Cell=factor(Cell))
+
+write.csv(Catch_Fishery,file=paste0(save_dir,"PS_Catch_25.csv"),row.names = FALSE)
+
+
+# sample weighted
+LF_raw_cell <- left_join(LF_raw,Catch) %>%
+  # mutate(LF_raised = LF * Catch) %>% # catch weighted
+  mutate(LF_raised = LF) %>%
+  select(year,quarter,lat,lon,Cell,Length,LF_raised)
+
+# combine the flag with catch data and then group catch
+LF_Fishery <- LF_raw_cell %>%
+  group_by(Cell,year,quarter,Length) %>%
+  summarise(lf_raised=sum(LF_raised),
+            n=length(unique(c(lat,lon)))) %>%
+  group_by(Cell,year,quarter) %>%
+  mutate(lf_sum=sum(lf_raised)) %>%
+  filter(lf_sum > 0) %>%
+  mutate(lf=lf_raised/lf_sum) %>%
+  select(Cell,year,quarter,n,Length,lf) %>%
+  spread(Length,lf)
+
+write.csv(LF_Fishery,file=paste0(save_dir,"PS_LF_25.csv"),row.names = FALSE)
+
+
+# catch weighted
+LF_raw_cell <- left_join(LF_raw,Catch) %>%
+  mutate(LF_raised = LF * Catch) %>% # catch weighted
+  # mutate(LF_raised = LF) %>%
+  select(year,quarter,lat,lon,Cell,Length,LF_raised)
+
+# combine the flag with catch data and then group catch
+LF_Fishery <- LF_raw_cell %>%
+  group_by(Cell,year,quarter,Length) %>%
+  summarise(lf_raised=sum(LF_raised),
+            n=length(unique(c(lat,lon)))) %>%
+  group_by(Cell,year,quarter) %>%
+  mutate(lf_sum=sum(lf_raised)) %>%
+  filter(lf_sum > 0) %>%
+  mutate(lf=lf_raised/lf_sum) %>%
+  select(Cell,year,quarter,n,Length,lf) %>%
+  spread(Length,lf)
+
+write.csv(LF_Fishery,file=paste0(save_dir,"PS_LF_25_cw.csv"),row.names = FALSE)
+
+# sample size
+LF_raw <- LF_DF %>%
+  select(Year,Lat,Lon,Length,LF) %>%
+  rename(lat=Lat,lon=Lon) %>%
+  group_by(Year,lat,lon) %>%
+  mutate(tot=sum(LF)) %>%
+  filter(tot>0)
+
+reg <- function(Lat,Lon) {
+  Region <- rep(2,length(Lat))
+  
+  Region[which(Lat>(-10)&Lon<60)] <- 1
+  Region[which(Lat>(-15)&Lon<75&Lon>60)] <- 1
+  
+  Region[which(Lat>(-15)&Lon>75)] <- 4
+  
+  Region[which(Lat<(-30)&Lon>40)] <- 3
+  Region[which(Lat>(-30)&Lat<(-15)&Lon>60)] <- 3
+  
+  return(Region)
+}
+
+LF_raw$Region <- reg(Lat=LF_raw$lat,Lon=LF_raw$lon)
+
+N <- LF_raw %>% mutate(Cell=Region) %>%
+  group_by(Cell,Year) %>%
+  summarise(n=length(unique(c(lat,lon))))
+
+write.csv(N,file=paste0(save_dir,"PS_25_N.csv"),row.names = FALSE)
+
+}
---
+++ Code/Loop/VAST_LF_Process_25.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+for (i in 1:10) {
+data <- read.csv(paste0("D:/OneDrive - IATTC/IATTC/2021/Spatial-SA/VAST_LF/Loop-Copy/",i,"/Table_for_SS3.csv"))
+
+LF <- data %>% mutate(Length=Category*10+30) %>%
+  group_by(Year) %>% mutate(Index=sum(Estimate_metric_tons)) %>%
+  filter(Index>0) %>%
+  mutate(LF=Estimate_metric_tons/Index) %>%
+  select(Year,Length,LF) %>%
+  spread(Length,LF)
+
+Nsamp <- read.csv(paste0("D:/OneDrive - IATTC/IATTC/2021/Spatial-SA/VAST_LF/Loop-Copy/",i,"/Nsamp.csv"))
+
+LF_SS <- data.frame(method=1,
+                    Yr=LF$Year,
+                    Seas=1,
+                    Flt=17,
+                    Gender=0,
+                    Part=0,
+                    Nsamp=Nsamp$N*5)
+
+LF_SS <- cbind(LF_SS,LF[,-1])
+
+write.csv(LF_SS,file=paste0("D:/OneDrive - IATTC/IATTC/2021/Spatial-SA/VAST_LF/Loop-Copy/",i,"/SS.csv"),row.names = FALSE)
+
+}
